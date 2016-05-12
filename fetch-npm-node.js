8d8da7a5f54db5b6109439c7bfb1c0471c3073af
Vu Nguyen <tuanvu.vn007@gmail.com>
2016-05-12 14:51:43 +0700

    add cache for node fetch

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,16 +1,45 @@
-"use strict";
+'use strict'
 
-var realFetch = require('node-fetch');
-module.exports = function(url, options) {
-	if (/^\/\//.test(url)) {
-		url = 'https:' + url;
-	}
-	return realFetch.call(this, url, options);
-};
+var realFetch = require('node-fetch')
+var redis = require('redis')
+var bluebird = require('bluebird')
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+var client = redis.createClient() // creates a new client
+
+client.on('connect', function () {
+  console.log('connected')
+})
+
+module.exports = function (url, options) {
+  var self = this
+  if (/^\/\//.test(url)) {
+    url = 'https:' + url
+  }
+
+  var key = url + JSON.stringify(options)
+
+  return client.getAsync(key, function (reply) {
+    if (reply) {
+      return reply
+    } else {
+      return realFetch.call(self, url, options)
+        .then(function (data) {
+          // save to redis
+          client.set(key, data)
+          client.expire(key, 120)
+
+          return data
+        })
+    }
+  })
+}
 
 if (!global.fetch) {
-	global.fetch = module.exports;
-	global.Response = realFetch.Response;
-	global.Headers = realFetch.Headers;
-	global.Request = realFetch.Request;
+  global.fetch = module.exports
+  global.Response = realFetch.Response
+  global.Headers = realFetch.Headers
+  global.Request = realFetch.Request
 }
